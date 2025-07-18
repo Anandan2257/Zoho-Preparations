cc177050c7e1aedf6cf6d7d1b92e844d2fe5a38a
Anandan2257 <periasamy312004@gmail.com>
2025-07-18 17:33:38 +0530

    commit

--- Assignment-1/Q1.java
+++ Assignment-1/Q1.java
@@ -1,8 +1,8 @@
-public class ScopeOfVariables{
+public class Q1{
     static int a = 10; //static variable
     int b = 12; //instance variable
     public static void main(String [] args){
-        ScopeOfVariables sc = new ScopeOfVariables();
+        Q1 sc = new Q1();
         int c = 12; //Local 
         final int d = 10; //final
 
