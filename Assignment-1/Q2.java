8d4f0aebe92caae06f2a9bb1352762b3525edaf4
Anandan2257 <periasamy312004@gmail.com>
2025-07-18 18:27:32 +0530

    commit

---
+++ Assignment-1/Q2.java
@@ -0,0 +1,13 @@
+public class Q2 {
+    public static void main(String arg []){
+        byte b = 10;
+        int a = 10;
+        char ch = 'a';
+        String str = "Anandan";
+        float fl = 12f;
+        double d = 12.33;
+        long lo = 112L;
+        System.out.print(b+ "\t" + a +" \t" + ch + "\t" + str + "\t" + fl + "\t" + d + "\t" + lo);
+    }
+    
+}
