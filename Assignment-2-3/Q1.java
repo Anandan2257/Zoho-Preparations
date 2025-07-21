8b4dd2a06aa034d9522d68d02c0e3e7463b43416
Anandan2257 <periasamy312004@gmail.com>
2025-07-21 16:28:54 +0530

    new

--- Assignment-2-3/Q1.java
+++ Assignment-2-3/Q1.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Q1 {
-        public static void main(String[] args) {
-            
+        public static void main(String ...args) {
+                Scanner sc = new Scanner(System.in);
+                int a = sc.nextInt();
+                StringBuilder str = new StringBuilder();
+                while(a > 0){
+                        a--;
+                        int b = a % 26;
+                        str.insert(0,(char)(b + 'A'));
+                        a = a / 26;
+                }
+                System.out.println(str);
+
+                
+        
         }
 }
