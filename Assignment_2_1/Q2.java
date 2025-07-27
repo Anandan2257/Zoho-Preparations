52386e9439a9c5d919ee57dad1cec38aae26be2f
Anandan2257 <periasamy312004@gmail.com>
2025-07-27 23:09:03 +0530

    new

--- Assignment_2_1/Q2.java
+++ Assignment_2_1/Q2.java
@@ -1,10 +1,20 @@
 public class Q2 {
     public static void main(String[] args) {
-        int n = 3;
-        for(int i = 0; i < ((n * 2) - 1) + 1; i++){
-            int value = i < n ? i + 1 : 2 * n - 1 - i;
-            for(int j = 0; j < value; j++){
-                System.out.print("* ");
+        int n = 5;
+        for(int i=0;i<n;i++){
+
+            for(int j=0;j<n;j++){
+                if(i==j){
+                    System.out.print("1 ");
+                }
+                else if(i==(n-j-1))
+                {
+                     System.out.print("1 ");
+                }
+                else{
+                    System.out.print(" ");
+                }
+
             }
             System.out.println();
         }
--- Assignment_2_1/Q3.java
+++ Assignment_2_1/Q3.java
@@ -1,5 +1,18 @@
 public class Q3 {
     public static void main(String[] args) {
+        int n = 3;
+        for(int i = 0; i < ((n * 2) - 1) + 1; i++){
+            int value = i < n ? i + 1 : 2 * n - 1 - i;
+            for(int j = 0; j < value; j++){
+                System.out.print("W ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        //pascal triangle
+
         int a = 5;
         for(int i = 0; i < a;i++){
             for(int space = 0; space < a - i - 1;space++){
@@ -15,4 +28,8 @@ public class Q3 {
             System.out.println(" ");
         }
     }
+
+    
+
+
 }
