608019ad8bb66443d603581aa393364ff13e755e
Anandan2257 <periasamy312004@gmail.com>
2025-07-28 22:28:54 +0530

    new

---
+++ Assignment_7/Bank/Main.java
@@ -0,0 +1,34 @@
+class Customer{
+
+}
+
+class Account{
+
+}
+
+class RBI{
+    Customer c; 
+    Account a; 
+    public double GetInterestRate() { }
+    public double GetWithdrawalLimit() { }
+}
+
+class SBI extends RBI{
+
+}
+
+class ICICI extends RBI{
+
+}
+
+class PNB extends RBI{
+
+}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
