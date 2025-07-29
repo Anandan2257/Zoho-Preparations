c5e25fa2388980b76915c58dd116e43829c61dd1
Anandan2257 <periasamy312004@gmail.com>
2025-07-29 18:52:23 +0530

    new

---
+++ Assignment_8/Singleton.java
@@ -0,0 +1,32 @@
+class SingletonClass{
+    static SingletonClass s = null; // create object...
+
+    private SingletonClass(){
+
+    }
+
+    public static SingletonClass getSingletonClass(){
+        if(s == null){
+            s = new SingletonClass();
+        }
+        return s;
+    }
+}
+
+public class Singleton {
+    public static void main(String[] args) {
+        SingletonClass s1 = SingletonClass.getSingletonClass();
+        SingletonClass s2 = SingletonClass.getSingletonClass();
+
+        System.out.println("Same Class SingletonClass: "+ s1 + " == " + s2);
+        newSingleton n = new newSingleton();
+        n.display();
+    }
+}
+
+class newSingleton{
+    void display(){
+         SingletonClass s3 = SingletonClass.getSingletonClass();
+         System.out.println("New Singleton: " + s3);
+    }
+}
