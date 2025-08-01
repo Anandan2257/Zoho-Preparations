f92f2c65f52a6805af054bcb5c88b51605f718f9
Anandan2257 <periasamy312004@gmail.com>
2025-08-01 18:44:09 +0530

    new

---
+++ Assignment_12/Q5/Q5.java
@@ -0,0 +1,10 @@
+/*Implement a basic producer-consumer problem using wait() and notify().
+--->Producer thread should add items to a shared buffer.
+--->Consumer thread should remove items.
+Ensure the buffer size is limited to 5 items.. Use Threads to implement the ATM machine, where 
+create thread to check the PIN number, another thread to perform the cash withdrawal, another 
+one to check the balance amount and print the receipt. */
+
+public class Q5 {
+    
+}
