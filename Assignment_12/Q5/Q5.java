3dc8c4b5e72c577a096fa60dd5c3e56274b8baad
Anandan2257 <periasamy312004@gmail.com>
2025-08-02 10:04:35 +0530

    new

--- Assignment_12/Q5/Q5.java
+++ Assignment_12/Q5/Q5.java
@@ -5,6 +5,22 @@ Ensure the buffer size is limited to 5 items.. Use Threads to implement the ATM 
 create thread to check the PIN number, another thread to perform the cash withdrawal, another 
 one to check the balance amount and print the receipt. */
 
+class Product{
+    public synchronized void add(){
+
+    }
+}
+
+class Consumer implements Runnable{
+
+    public void run(){
+        
+    }
+    public synchronized void remove(){
+
+    }
+    
+}
 public class Q5 {
     public static void main(String[] args) {
         
