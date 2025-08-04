f0abd64b5e4796c8f928adae1b72a6a439f6f730
Anandan2257 <periasamy312004@gmail.com>
2025-08-04 15:46:36 +0530

    new

--- Assignment_10/Q8/Q8.java
+++ Assignment_10/Q8/Q8.java
@@ -1,3 +1,16 @@
+/* Develop a Java program to manage a list of bank accounts using ArrayList.
+--->Create an Account class with the following attributes:
+a. accountNumber (int)
+b. holderName (String)
+c. balance (double)
+--->Use autoboxing to store the balance and interest as Double wrapper objects.
+--->Use manual boxing to convert a primitive interest rate into a Double object.
+--->Unbox the values (both automatic and manual) to calculate and update the new balance.
+--->Add at least three Account objects to an ArrayList<Account>.
+--->For each account:
+a. Display the holder name, account number, original balance
+b. Apply 5% interest
+c. Show the new balance using primitive values (unboxed). */
 import java.util.*;
 class Account{
     int accountNumber;
@@ -15,7 +28,7 @@ class Account{
     }
 
     void newBalance(double interest){
-        Double newBalance = balance + (balance + interest); //autoboking
+        Double newBalance = balance + (balance + interest); //autoboxing
         System.out.println("New Balance After Adding 4 %: " + this.balance * 4);
     }
 }
@@ -25,7 +38,7 @@ public class Q8 {
         ArrayList<Account> bankDetails = new ArrayList<>();
 
         double interestRatePrimitive = 0.04;
-        Double interestRate = Double.valueOf(interestRatePrimitive); //Boking
+        Double interestRate = Double.valueOf(interestRatePrimitive); //Boxing
 
         bankDetails.add(new Account(101,"Anandan", 100000));
         bankDetails.add(new Account(102,"Abi", 150000));
