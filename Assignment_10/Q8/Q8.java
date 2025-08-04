ca656f7a49dd86de08e5daf7a48029a410d0e781
Anandan2257 <periasamy312004@gmail.com>
2025-08-04 15:44:57 +0530

    new

---
+++ Assignment_10/Q8/Q8.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Account{
+    int accountNumber;
+    String holderName;
+    double balance;
+
+    Account(int accountNumber,String holderName,double balance){
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    void display(){
+        System.out.println("Acoount number: " + this.accountNumber + " Holder Name: " + this.holderName + " Balance: " + this.balance);
+    }
+
+    void newBalance(double interest){
+        Double newBalance = balance + (balance + interest);
+        System.out.println("New Balance After Adding 4 %: " + this.balance * 4);
+    }
+}
+
+public class Q8 {
+    public static void main(String[] args) {
+        ArrayList<Account> bankDetails = new ArrayList<>();
+
+        double interestRatePrimitive = 0.04;
+        Double interestRate = Double.valueOf(interestRatePrimitive);
+
+        bankDetails.add(new Account(101,"Anandan", 100000));
+        bankDetails.add(new Account(102,"Abi", 150000));
+        bankDetails.add(new Account(104,"Tamil", 160000));
+
+        System.out.println("Before Applying Interest: ");
+
+        for(Account acc : bankDetails){
+            acc.display();
+        }
+
+        System.out.println("After Applying Interest:");
+
+        for(Account acc : bankDetails){
+            acc.newBalance(interestRatePrimitive);
+            acc.display();
+        }
+    }
+}
