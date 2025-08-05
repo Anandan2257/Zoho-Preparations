dd1ef9b238e05e7d44d970ea5f23fc833d2d9858
Anandan2257 <periasamy312004@gmail.com>
2025-08-05 09:32:08 +0530

    new

--- Assignment_11/Q2/Q2.java
+++ Assignment_11/Q2/Q2.java
@@ -1,5 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.Buffer;
+
 public class Q2 {
     public static void main(String[] args) {
-        
+        try{
+        FileWriter fw = new FileWriter("new.txt",true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        for(String content: args){
+            bw.write(content + " ");
+        }
+        bw.newLine();
+        bw.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+        try{
+            FileReader fr = new FileReader("new.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String line = br.readLine();
+            while(line != null){
+                
+            }
+
+        }
     }
 }
