4c845c28f8f30c9bb53b18bca07aad1d1add1cff
Anandan2257 <periasamy312004@gmail.com>
2025-08-11 16:29:29 +0530

    new

--- Assignment_12/Q5/Q5.java
+++ Assignment_12/Q5/Q5.java
@@ -1,28 +1,97 @@
 /*Implement a basic producer-consumer problem using wait() and notify().
 --->Producer thread should add items to a shared buffer.
 --->Consumer thread should remove items.
-Ensure the buffer size is limited to 5 items.. Use Threads to implement the ATM machine, where 
-create thread to check the PIN number, another thread to perform the cash withdrawal, another 
-one to check the balance amount and print the receipt. */
+ */
 
-class Product{
+class Count{
+    int count = 0;
+    int max = 20;
     public synchronized void add(){
+        for(int i = 1 ; i <= 20; i++){
+           while(count >= max){ 
+                try{
+                    wait();
+                }catch(Exception e){
+                    System.out.println(e);
+            }
+             }
+             count++;
+            System.out.println("Count Add: " + count);
+            try{
+                Thread.sleep(500);
+            }catch(Exception e){
+                System.out.println(e);
+            }
+            notifyAll();
+        }
+        }
 
+
+    public synchronized void remove(){
+        for(int i = 20 ; i >= 1; i--){
+            while(count <= 0){
+                try{
+                    wait();
+                }catch(Exception e){
+                    System.out.println(e);
+            }
+        }
+            System.out.println("Count Remove: " + count);
+            count--;
+            try{
+                Thread.sleep(500);
+            }catch(Exception e){
+                System.out.println(e);
+            }
+            notifyAll();
+        }
+        }
+    }
+
+
+class Product implements Runnable{
+    Count count;
+
+    Product(Count count){
+        this.count = count;
+    }
+    public void run(){
+        count.add();
     }
 }
 
 class Consumer implements Runnable{
 
-    public void run(){
-        
-    }
-    public synchronized void remove(){
+    Count count;
 
+    Consumer(Count count){
+        this.count = count;
     }
+    public void run(){
+        count.remove();
+    }
+    
     
 }
 public class Q5 {
     public static void main(String[] args) {
-        
+        Count count = new Count();
+
+        Product p = new Product(count);
+        Thread add = new Thread(p);
+
+        Consumer c = new Consumer(count);
+        Thread remove = new Thread(c);
+
+        add.start();
+        remove.start();
+        try{
+            add.join();
+            remove.join();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+
     }
 }
